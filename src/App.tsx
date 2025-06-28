4d0b46e36612cb1c7f858769f86e49864e29bc06
shun-radiation <mizokami.shunsuke.it@gmail.com>
2025-06-28 17:10:16 +0900

    applayoutを作成、<Outlet />

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import './App.css';
 import Home from './pages/Home';
 import Report from './pages/Report';
 import NoMatch from './pages/NoMatch';
+import AppLayout from './components/layout/AppLayout';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/report' element={<Report />} />
-        <Route path='*' element={<NoMatch />} />
+        <Route path='/' element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path='/report' element={<Report />} />
+          <Route path='*' element={<NoMatch />} />
+        </Route>
       </Routes>
     </Router>
   );
