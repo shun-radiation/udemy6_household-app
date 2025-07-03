19930bf4daac112d577aa1750d9c51a9989afbd2
shun-radiation <mizokami.shunsuke.it@gmail.com>
2025-07-03 22:59:50 +0900

    firestoreからデータを取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
@@ -7,8 +8,31 @@ import AppLayout from './components/layout/AppLayout';
 import { theme } from './theme/theme';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
+import { type Transaction } from './types/index';
+import { db } from './firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 function App() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    const fecheTransactions = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'Transactions'));
+        const transactionsData = querySnapshot.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction;
+        });
+        console.log(transactionsData);
+        setTransactions(transactionsData);
+      } catch (err) {
+        // error
+      }
+    };
+    fecheTransactions();
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
