db6b97981abbf4d4c75f7b654e9de01cc13b16bd
shun-radiation <mizokami.shunsuke.it@gmail.com>
2025-08-02 01:46:13 +0900

    date pickerを日本語化

--- src/components/MonthSelector.tsx
+++ src/components/MonthSelector.tsx
@@ -2,17 +2,28 @@ import { Box, Button } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { ja } from 'date-fns/locale';
 
 const MonthSelector = () => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <LocalizationProvider
+      dateAdapter={AdapterDateFns}
+      adapterLocale={ja}
+      // dateFormats={{ monthAndYear: 'yyyy年 MM月' }}
+    >
       <Box
         sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
       >
         <Button color={'error'} variant='contained'>
           先月
         </Button>
-        <DatePicker sx={{ mx: 2 }} />
+        <DatePicker
+          label='年月を選択'
+          sx={{ mx: 2, background: 'white' }}
+          views={['year', 'month']}
+          format='yyyy/MM'
+          // slotProps={{ toolbar: { toolbarFormat: 'yyyy年MM月' } }}
+        />
         <Button color={'primary'} variant='contained'>
           次月
         </Button>
