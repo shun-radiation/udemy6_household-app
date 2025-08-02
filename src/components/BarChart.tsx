55292cf6a8d67bab1aa3995a624cf7f5ff823ffe
shun-radiation <mizokami.shunsuke.it@gmail.com>
2025-08-03 02:06:19 +0900

    react-chartjs-2 vertical bar chartを利用して仮データで棒グラフを表示

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const BarChart = () => {
-  return <div>BarChart</div>;
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+
+  const labels = [
+    '2025-08-01',
+    '2025-08-02',
+    '2025-08-03',
+    '2025-08-04',
+    '2025-08-05',
+    '2025-08-06',
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: '支出',
+        data: [100, 200, 300, 400, 500, 600],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: '収入',
+        data: [600, 500, 400, 300, 200, 100],
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+  return <Bar options={options} data={data} />;
 };
 
 export default BarChart;
