ffe6d176f379ff9703058b6f2c662bf688363ede
shun-radiation <mizokami.shunsuke.it@gmail.com>
2025-08-12 03:39:21 +0900

    Homeコンポーネントにグローバルな値と月間の取引データを反映

--- src/App.tsx
+++ src/App.tsx
@@ -25,106 +25,106 @@ import { AppContextProvider, useAppContext } from './context/AppContext';
 import { isFireStoreError } from './utils/errorHandling';
 
 function App() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-  const [currentMonth, setCurrentMonth] = useState(new Date());
-  const [isLoading, setIsLoading] = useState(true);
+  // const [transactions, setTransactions] = useState<Transaction[]>([]);
+  // const [currentMonth, setCurrentMonth] = useState(new Date());
+  // const [isLoading, setIsLoading] = useState(true);
 
   // console.log(currentMonth);
   // console.log(format(currentMonth, 'yyyy-MM'));
 
-  // ひと月分のデータを取得
-  const monthlyTransactions = transactions.filter((transaction) => {
-    return transaction.date.startsWith(formatMonth(currentMonth));
-  });
+  // // ひと月分のデータを取得
+  // const monthlyTransactions = transactions.filter((transaction) => {
+  //   return transaction.date.startsWith(formatMonth(currentMonth));
+  // });
 
-  // 取引を保存する処理
-  const handleSaveTransaction = async (transaction: Schema) => {
-    try {
-      console.log(transaction);
-      // firestoreにデータを保存
-      // Add a new document with a generated id.
-      const docRef = await addDoc(collection(db, 'Transactions'), transaction);
-      console.log('Document written with ID: ', docRef.id);
-      const newTransaction = {
-        id: docRef.id,
-        ...transaction,
-      } as Transaction;
-      console.log(newTransaction);
-      setTransactions((prevTransactions) => [
-        ...prevTransactions,
-        newTransaction,
-      ]);
-    } catch (err) {
-      // error
-      if (isFireStoreError(err)) {
-        console.error('firebaseのエラーは', err);
-        console.error('firebaseのエラーメッセージは', err.message);
-        console.error('firebaseのエラーコードは', err.code);
-      } else {
-        console.error('一般的なエラーは', err);
-      }
-    }
-  };
+  // // 取引を保存する処理
+  // const handleSaveTransaction = async (transaction: Schema) => {
+  //   try {
+  //     console.log(transaction);
+  //     // firestoreにデータを保存
+  //     // Add a new document with a generated id.
+  //     const docRef = await addDoc(collection(db, 'Transactions'), transaction);
+  //     console.log('Document written with ID: ', docRef.id);
+  //     const newTransaction = {
+  //       id: docRef.id,
+  //       ...transaction,
+  //     } as Transaction;
+  //     console.log(newTransaction);
+  //     setTransactions((prevTransactions) => [
+  //       ...prevTransactions,
+  //       newTransaction,
+  //     ]);
+  //   } catch (err) {
+  //     // error
+  //     if (isFireStoreError(err)) {
+  //       console.error('firebaseのエラーは', err);
+  //       console.error('firebaseのエラーメッセージは', err.message);
+  //       console.error('firebaseのエラーコードは', err.code);
+  //     } else {
+  //       console.error('一般的なエラーは', err);
+  //     }
+  //   }
+  // };
 
-  // 削除処理
-  const handleDeleteTransaction = async (
-    transactionIds: string | readonly string[]
-  ) => {
-    try {
-      // オブジェクト(複数)ならそのまま、単一ならオブジェクト化する。
-      const idsToDelete = Array.isArray(transactionIds)
-        ? transactionIds
-        : [transactionIds];
-      console.log('削除対象', idsToDelete);
+  // // 削除処理
+  // const handleDeleteTransaction = async (
+  //   transactionIds: string | readonly string[]
+  // ) => {
+  //   try {
+  //     // オブジェクト(複数)ならそのまま、単一ならオブジェクト化する。
+  //     const idsToDelete = Array.isArray(transactionIds)
+  //       ? transactionIds
+  //       : [transactionIds];
+  //     console.log('削除対象', idsToDelete);
 
-      for (const id of idsToDelete) {
-        // firestoreのデータ削除
-        await deleteDoc(doc(db, 'Transactions', id));
-      }
-      // リロードなしですぐに画面に反映されるために
-      const filterdTransactions = transactions.filter(
-        (transaction) => !idsToDelete.includes(transaction.id)
-      );
-      // console.log(filterdTransactions);
-      setTransactions(filterdTransactions);
-    } catch (err) {
-      // error
-      if (isFireStoreError(err)) {
-        console.error('firebaseのエラーは', err);
-        console.error('firebaseのエラーメッセージは', err.message);
-        console.error('firebaseのエラーコードは', err.code);
-      } else {
-        console.error('一般的なエラーは', err);
-      }
-    }
-  };
+  //     for (const id of idsToDelete) {
+  //       // firestoreのデータ削除
+  //       await deleteDoc(doc(db, 'Transactions', id));
+  //     }
+  //     // リロードなしですぐに画面に反映されるために
+  //     const filterdTransactions = transactions.filter(
+  //       (transaction) => !idsToDelete.includes(transaction.id)
+  //     );
+  //     // console.log(filterdTransactions);
+  //     setTransactions(filterdTransactions);
+  //   } catch (err) {
+  //     // error
+  //     if (isFireStoreError(err)) {
+  //       console.error('firebaseのエラーは', err);
+  //       console.error('firebaseのエラーメッセージは', err.message);
+  //       console.error('firebaseのエラーコードは', err.code);
+  //     } else {
+  //       console.error('一般的なエラーは', err);
+  //     }
+  //   }
+  // };
 
-  // 変更処理
-  const handleUpdateTransaction = async (
-    transaction: Schema,
-    transactionId: string
-  ) => {
-    try {
-      // firestore更新処理
-      const docRef = doc(db, 'Transactions', transactionId);
-      await updateDoc(docRef, transaction);
-      // フロント更新
-      const updatedTransactions = transactions.map((t) =>
-        t.id === transactionId ? { ...t, ...transaction } : t
-      ) as Transaction[];
-      // console.log(updatedTransactions);
-      setTransactions(updatedTransactions);
-    } catch (err) {
-      // error
-      if (isFireStoreError(err)) {
-        console.error('firebaseのエラーは', err);
-        console.error('firebaseのエラーメッセージは', err.message);
-        console.error('firebaseのエラーコードは', err.code);
-      } else {
-        console.error('一般的なエラーは', err);
-      }
-    }
-  };
+  // // 変更処理
+  // const handleUpdateTransaction = async (
+  //   transaction: Schema,
+  //   transactionId: string
+  // ) => {
+  //   try {
+  //     // firestore更新処理
+  //     const docRef = doc(db, 'Transactions', transactionId);
+  //     await updateDoc(docRef, transaction);
+  //     // フロント更新
+  //     const updatedTransactions = transactions.map((t) =>
+  //       t.id === transactionId ? { ...t, ...transaction } : t
+  //     ) as Transaction[];
+  //     // console.log(updatedTransactions);
+  //     setTransactions(updatedTransactions);
+  //   } catch (err) {
+  //     // error
+  //     if (isFireStoreError(err)) {
+  //       console.error('firebaseのエラーは', err);
+  //       console.error('firebaseのエラーメッセージは', err.message);
+  //       console.error('firebaseのエラーコードは', err.code);
+  //     } else {
+  //       console.error('一般的なエラーは', err);
+  //     }
+  //   }
+  // };
 
   return (
     <AppContextProvider>
@@ -137,11 +137,11 @@ function App() {
                 index
                 element={
                   <Home
-                    monthlyTransactions={monthlyTransactions}
-                    setCurrentMonth={setCurrentMonth}
-                    onSaveTransaction={handleSaveTransaction}
-                    onDeleteTransaction={handleDeleteTransaction}
-                    onUpdateTransaction={handleUpdateTransaction}
+                  // monthlyTransactions={monthlyTransactions}
+                  // setCurrentMonth={setCurrentMonth}
+                  // onSaveTransaction={handleSaveTransaction}
+                  // onDeleteTransaction={handleDeleteTransaction}
+                  // onUpdateTransaction={handleUpdateTransaction}
                   />
                 }
               />
@@ -149,11 +149,11 @@ function App() {
                 path='/report'
                 element={
                   <Report
-                    currentMonth={currentMonth}
-                    setCurrentMonth={setCurrentMonth}
-                    monthlyTransactions={monthlyTransactions}
-                    isLoading={isLoading}
-                    onDeleteTransaction={handleDeleteTransaction}
+                  // currentMonth={currentMonth}
+                  // setCurrentMonth={setCurrentMonth}
+                  // monthlyTransactions={monthlyTransactions}
+                  // isLoading={isLoading}
+                  // onDeleteTransaction={handleDeleteTransaction}
                   />
                 }
               />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,137 +4,148 @@ import Calendar from '../components/Calendar';
 import TransactionMenu from '../components/TransactionMenu';
 import TransactionForm from '../components/TransactionForm';
 import { type Transaction } from '../types/index';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { format } from 'date-fns';
 import type { Schema } from '../validations/schema';
 import type { DateClickArg } from '@fullcalendar/interaction/index.js';
+import { useAppContext } from '../context/AppContext';
+import useMonthlyTransactions from '../hooks/useMonthlyTransactions';
 
-interface HomeProps {
-  monthlyTransactions: Transaction[];
-  setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>;
-  onSaveTransaction: (transaction: Schema) => Promise<void>;
-  onDeleteTransaction: (transactionId: string) => Promise<void>;
-  onUpdateTransaction: (
-    transaction: Schema,
-    transactionId: string
-  ) => Promise<void>;
-}
+// interface HomeProps {
+//   monthlyTransactions: Transaction[];
+//   setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>;
+//   onSaveTransaction: (transaction: Schema) => Promise<void>;
+//   onDeleteTransaction: (transactionId: string) => Promise<void>;
+//   onUpdateTransaction: (
+//     transaction: Schema,
+//     transactionId: string
+//   ) => Promise<void>;
+// }
 
-const Home = ({
-  monthlyTransactions,
-  setCurrentMonth,
-  onSaveTransaction,
-  onDeleteTransaction,
-  onUpdateTransaction,
-}: HomeProps) => {
-  const today = format(new Date(), 'yyyy-MM-dd');
-  // console.log(today);
-  const [currentDay, setCurrentDay] = useState(today);
-  const [isEntryDrawerOpen, setIsEntryDrawerOpen] = useState(false);
-  const [selectedTransaction, setSelectedTransaction] =
-    useState<Transaction | null>(null);
-  const [isMobileDrawerOpen, setIsMobileDrawerOpen] = useState(false);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
+const Home = () =>
+  //   {
+  //   monthlyTransactions,
+  //   setCurrentMonth,
+  //   onSaveTransaction,
+  //   onDeleteTransaction,
+  //   onUpdateTransaction,
+  // }: HomeProps
+  {
+    const { isMobile } = useAppContext();
+    const monthlyTransactions = useMonthlyTransactions();
 
-  // ブレイクポイントの設定
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('lg')); // 1200px(lg)以下かどうかboolean
-  console.log('isMobile', isMobile);
+    const today = format(new Date(), 'yyyy-MM-dd');
+    // console.log(today);
+    const [currentDay, setCurrentDay] = useState(today);
+    const [isEntryDrawerOpen, setIsEntryDrawerOpen] = useState(false);
+    const [selectedTransaction, setSelectedTransaction] =
+      useState<Transaction | null>(null);
+    const [isMobileDrawerOpen, setIsMobileDrawerOpen] = useState(false);
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  // 一日分のデータを取得
-  const dailyTransactions = monthlyTransactions.filter((transaction) => {
-    // console.log(currentDay);
-    return transaction.date === currentDay;
-  });
-  // console.log(dailyTransactions);
+    // // ブレイクポイントの設定
+    // const theme = useTheme();
+    // const isMobile = useMediaQuery(theme.breakpoints.down('lg')); // 1200px(lg)以下かどうかboolean
+    // console.log('isMobile', isMobile);
 
-  // フォームclose処理
-  const onCloseForm = () => {
-    setSelectedTransaction(null);
-    if (isMobile) {
-      setIsDialogOpen((prev) => !prev);
-    } else {
-      setIsEntryDrawerOpen((prev) => !prev);
-    }
-  };
-  // フォームopen処理(「内訳を追加」ボタン)
-  const handleAddTransactionForm = () => {
-    if (isMobile) {
-      setIsDialogOpen(true);
-    } else {
-      if (selectedTransaction) {
-        setSelectedTransaction(null);
+    // 一日分のデータを取得
+    const dailyTransactions = useMemo(() => {
+      return monthlyTransactions.filter((transaction) => {
+        // console.log(currentDay);
+        transaction.date === currentDay;
+      });
+    }, [monthlyTransactions, currentDay]);
+    // console.log(dailyTransactions);
+
+    // フォームclose処理
+    const onCloseForm = () => {
+      setSelectedTransaction(null);
+      if (isMobile) {
+        setIsDialogOpen((prev) => !prev);
       } else {
         setIsEntryDrawerOpen((prev) => !prev);
       }
-    }
-  };
+    };
+    // フォームopen処理(「内訳を追加」ボタン)
+    const handleAddTransactionForm = () => {
+      if (isMobile) {
+        setIsDialogOpen(true);
+      } else {
+        if (selectedTransaction) {
+          setSelectedTransaction(null);
+        } else {
+          setIsEntryDrawerOpen((prev) => !prev);
+        }
+      }
+    };
 
-  // 取引が選択された時の処理
-  const handleSelectTransaction = (transaction: Transaction) => {
-    if (isMobile) {
-      setIsDialogOpen(true);
-    } else {
-      setIsEntryDrawerOpen(true);
-    }
-    setSelectedTransaction(transaction);
-    console.log(transaction);
-  };
+    // 取引が選択された時の処理
+    const handleSelectTransaction = (transaction: Transaction) => {
+      if (isMobile) {
+        setIsDialogOpen(true);
+      } else {
+        setIsEntryDrawerOpen(true);
+      }
+      setSelectedTransaction(transaction);
+      console.log(transaction);
+    };
 
-  // モバイル用Drawerをcloseするための処理
-  const handleCloseMobileDrawer = () => {
-    setIsMobileDrawerOpen(false);
-  };
+    // モバイル用Drawerをcloseするための処理
+    const handleCloseMobileDrawer = () => {
+      setIsMobileDrawerOpen(false);
+    };
 
-  // 日付を選択した時の処理
-  const handleDateClick = (dateInfo: DateClickArg) => {
-    // console.log(dateInfo);
-    setCurrentDay(dateInfo.dateStr);
-    setIsMobileDrawerOpen(true);
-  };
+    // 日付を選択した時の処理
+    const handleDateClick = (dateInfo: DateClickArg) => {
+      // console.log(dateInfo);
+      setCurrentDay(dateInfo.dateStr);
+      setIsMobileDrawerOpen(true);
+    };
 
-  return (
-    <Box sx={{ display: 'flex' }}>
-      {/* 左側コンテンツ */}
-      <Box sx={{ flexGrow: 1, bgcolor: 'pink' }}>
-        <MonthlySummary monthlyTransactions={monthlyTransactions} />
-        <Calendar
-          monthlyTransactions={monthlyTransactions}
-          setCurrentMonth={setCurrentMonth}
-          currentDay={currentDay}
-          setCurrentDay={setCurrentDay}
-          today={today}
-          onDateClick={handleDateClick}
-        />
+    return (
+      <Box sx={{ display: 'flex' }}>
+        {/* 左側コンテンツ */}
+        <Box sx={{ flexGrow: 1, bgcolor: 'pink' }}>
+          <MonthlySummary
+          // monthlyTransactions={monthlyTransactions}
+          />
+          <Calendar
+            // monthlyTransactions={monthlyTransactions}
+            // setCurrentMonth={setCurrentMonth}
+            currentDay={currentDay}
+            setCurrentDay={setCurrentDay}
+            today={today}
+            onDateClick={handleDateClick}
+          />
+        </Box>
+
+        {/* 右側コンテンツ */}
+        <Box>
+          <TransactionMenu
+            dailyTransactions={dailyTransactions}
+            currentDay={currentDay}
+            handleAddTransactionForm={handleAddTransactionForm}
+            onSelectTransaction={handleSelectTransaction}
+            // isMobile={isMobile}
+            isMobileDrawerOpen={isMobileDrawerOpen}
+            handleCloseMobileDrawer={handleCloseMobileDrawer}
+          />
+          <TransactionForm
+            onCloseForm={onCloseForm}
+            isEntryDrawerOpen={isEntryDrawerOpen}
+            currentDay={currentDay}
+            // onSaveTransaction={onSaveTransaction}
+            // onDeleteTransaction={onDeleteTransaction}
+            // onUpdateTransaction={onUpdateTransaction}
+            selectedTransaction={selectedTransaction}
+            setSelectedTransaction={setSelectedTransaction}
+            // isMobile={isMobile}
+            isDialogOpen={isDialogOpen}
+            setIsDialogOpen={setIsDialogOpen}
+          />
+        </Box>
       </Box>
-
-      {/* 右側コンテンツ */}
-      <Box>
-        <TransactionMenu
-          dailyTransactions={dailyTransactions}
-          currentDay={currentDay}
-          handleAddTransactionForm={handleAddTransactionForm}
-          onSelectTransaction={handleSelectTransaction}
-          isMobile={isMobile}
-          isMobileDrawerOpen={isMobileDrawerOpen}
-          handleCloseMobileDrawer={handleCloseMobileDrawer}
-        />
-        <TransactionForm
-          onCloseForm={onCloseForm}
-          isEntryDrawerOpen={isEntryDrawerOpen}
-          currentDay={currentDay}
-          onSaveTransaction={onSaveTransaction}
-          selectedTransaction={selectedTransaction}
-          onDeleteTransaction={onDeleteTransaction}
-          setSelectedTransaction={setSelectedTransaction}
-          onUpdateTransaction={onUpdateTransaction}
-          isMobile={isMobile}
-          isDialogOpen={isDialogOpen}
-          setIsDialogOpen={setIsDialogOpen}
-        />
-      </Box>
-    </Box>
-  );
-};
+    );
+  };
 
 export default Home;
